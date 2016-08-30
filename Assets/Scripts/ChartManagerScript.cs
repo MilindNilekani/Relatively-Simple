63255ea5d162a02ef4d73fad9a62f5173ff890f3
physics <users@physicss-MBP.wv.cc.cmu.edu>
2016-08-30 16:22:20 -0400

    Removed old graph implementation from MainScene and MainSceneSpaceContraction

--- Assets/Scripts/ChartManagerScript.cs
+++ Assets/Scripts/ChartManagerScript.cs
@@ -5,16 +5,16 @@ using System.Collections.Generic;
 public class ChartManagerScript : MonoBehaviour {
 
     //public WMG_Axis_Graph lineGraph1;
-    public WMG_Axis_Graph lineGraph2;
+    //public WMG_Axis_Graph lineGraph2;
     //public WMG_Axis_Graph lineGraph3;
     public int accLogMemory;
     public int numberOfPoints;
 	public SimpleGraph lineGraph3;
-	public GameObject graphObj;
+	//public GameObject graphObj;
 
     //private WMG_Series series1;
     //private WMG_Series series12;
-    private WMG_Series series2;
+    //private WMG_Series series2;
     //private WMG_Series series3;
     //private WMG_Series series32;
 
@@ -32,26 +32,26 @@ public class ChartManagerScript : MonoBehaviour {
 	void Awake () {
         //series1 = lineGraph1.lineSeries[0].GetComponent<WMG_Series>();
         //series12 = lineGraph1.lineSeries[1].GetComponent<WMG_Series>();
-        series2 = lineGraph2.lineSeries[0].GetComponent<WMG_Series>();
+        //series2 = lineGraph2.lineSeries[0].GetComponent<WMG_Series>();
         //series3 = lineGraph3.lineSeries[0].GetComponent<WMG_Series>();
         //series32 = lineGraph3.lineSeries[1].GetComponent<WMG_Series>();
 
         //series1.pointValues = new List<Vector2>();
         //series12.pointValues = new List<Vector2>(); 
-        series2.pointValues = new List<Vector2>();
+        //series2.pointValues = new List<Vector2>();
         //series3.pointValues = new List<Vector2>();
         //series32.pointValues = new List<Vector2>();
 
         //lineGraph1.yAxisMaxValue = 15;
         //lineGraph1.yAxisMinValue = 0;
 
-        lineGraph2.yAxisMaxValue = 3;
-        lineGraph2.yAxisMinValue = -3;
+        //lineGraph2.yAxisMaxValue = 3;
+        //lineGraph2.yAxisMinValue = -3;
 
         //lineGraph3.yAxisMaxValue = 5;
         //lineGraph3.yAxisMinValue = -5;
 
-        lineGraph2.axesType = WMG_Axis_Graph.axesTypes.I_IV;
+        //lineGraph2.axesType = WMG_Axis_Graph.axesTypes.I_IV;
         //lineGraph3.axesType = WMG_Axis_Graph.axesTypes.I_IV;
 
         accLog = new List <Vector2>();
@@ -90,7 +90,7 @@ public class ChartManagerScript : MonoBehaviour {
 
 			//compactAccLog.Add (new Vector2 (i, Random.Range(0,10)));
         }
-        series2.pointValues = compactAccLog;
+        //series2.pointValues = compactAccLog;
 		lineGraph3.NewValues(compactAccLog);
         //series3.pointValues = userAccXLog;
         //series32.pointValues = userAccYLog;
