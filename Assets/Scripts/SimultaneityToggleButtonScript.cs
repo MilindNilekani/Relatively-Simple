74f8dc2421aa5b977c4affbba80b9877bf125782
physics <users@physicss-MBP.wv.cc.cmu.edu>
2016-09-02 17:46:46 -0400

    Start button toggles play/pause images when clickes in Simultaneity scene

---
+++ Assets/Scripts/SimultaneityToggleButtonScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SimultaneityToggleButtonScript : MonoBehaviour {
+
+	public Sprite playImage;
+	public Sprite stopImage;
+
+	private bool photonsOn;
+
+	// Use this for initialization
+	void Start () {
+		photonsOn = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnClick()
+	{
+		if (photonsOn) {
+			gameObject.GetComponent<Button> ().image.sprite = playImage;
+		} 
+		else {
+			gameObject.GetComponent<Button> ().image.sprite = stopImage;
+		}
+		photonsOn = !photonsOn;
+	}
+}
