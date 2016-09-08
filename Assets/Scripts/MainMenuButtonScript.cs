ae0ff86d806360c2ab7e47ddd0cb88e8b2627f67
physics <users@physicss-MBP.wv.cc.cmu.edu>
2016-09-08 15:35:44 -0400

    Implemented submenu system.

---
+++ Assets/Scripts/MainMenuButtonScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuButtonScript : MonoBehaviour {
+
+	public GameObject subMenuPrefab;
+
+	private GameObject subMenu;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnClick(){
+		subMenu = Instantiate(subMenuPrefab);
+		subMenu.transform.parent = transform.parent;
+		StartCoroutine ("OpenSubMenu");
+	}
+
+	IEnumerator OpenSubMenu()
+	{
+		for (float t = 0; t <= 1; t += 0.1f) {
+			subMenu.transform.localPosition = Vector3.Lerp (transform.localPosition, Vector3.zero, t);
+			subMenu.transform.localScale = Vector3.Lerp (Vector3.zero, Vector3.one, t);
+			yield return new WaitForSeconds (0.05f);
+		}
+	}
+}
--- Assets/Scripts/StartScreenScript.cs
+++ Assets/Scripts/StartScreenScript.cs
@@ -1,11 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class StartScreenScript : MonoBehaviour {
 
+	public Image timeDilationSubMenu;
+	public Image spaceContractionSubMenu;
+	public Image simultaneitySubMenu;
+
+	enum direction {IN, OUT};
+
+	Vector3 subMenuPos;
+
 	// Use this for initialization
 	void Start () {
-	
+		subMenuPos = timeDilationSubMenu.GetComponent<RectTransform> ().anchoredPosition;
+
 	}
 	
 	// Update is called once per frame
@@ -13,14 +23,66 @@ public class StartScreenScript : MonoBehaviour {
 	
 	}
 
-	public void StartGame()
+	public void OnTimeDilationButtonClick()
+	{
+		StartCoroutine (ToggleSubMenu(timeDilationSubMenu, direction.IN));
+	}
+
+	public void OnSpaceContractionClick()
+	{
+		StartCoroutine (ToggleSubMenu(spaceContractionSubMenu, direction.IN));
+	}
+
+	public void OnSimultaneityClick()
+	{
+		StartCoroutine (ToggleSubMenu(simultaneitySubMenu, direction.IN));
+	}
+
+	public void OnTimeDilationExitClick()
+	{
+		StartCoroutine (ToggleSubMenu(timeDilationSubMenu, direction.OUT));
+	}
+
+	public void OnSpaceContractionExitClick()
+	{
+		StartCoroutine (ToggleSubMenu(spaceContractionSubMenu, direction.OUT));
+	}
+
+	public void OnSimultaneityExitClick()
+	{
+		StartCoroutine(ToggleSubMenu(simultaneitySubMenu, direction.OUT));
+	}
+
+	IEnumerator ToggleSubMenu (Image a, direction dir)
+	{
+		Vector3 startPos;
+		Vector3 endPos;
+		if (dir == direction.IN) {
+			startPos = subMenuPos;
+			endPos = Vector3.zero;
+		}
+		else
+		{
+			startPos = Vector3.zero;
+			endPos = subMenuPos;
+		}
+
+		for (float t = 0f;t<=1.0f; t+=0.05f)
+		{
+			a.GetComponent<RectTransform> ().anchoredPosition = Vector3.Lerp(startPos, endPos, t);
+			yield return new WaitForSeconds(0.005f);
+		}
+		a.GetComponent<RectTransform> ().anchoredPosition = (dir == direction.IN)? Vector3.zero:subMenuPos;
+	}
+
+	public void StartSimulationTimeDilation()
 	{
 		Application.LoadLevel (2);
 	}
 
-	public void StartGameLengthcontraction()
+	public void StartSimulationLengthContraction()
 	{
-		Application.LoadLevel (7);
+		Application.LoadLevel (6);
 	}
 
 	public void HelpScreen()
@@ -28,14 +90,14 @@ public class StartScreenScript : MonoBehaviour {
 		Application.LoadLevel (3);
 	}
 
-    public void FeynmansExperimentScreen()
+    public void StartFeynmansExperiment()
     {
-        Application.LoadLevel(5);
+        //Application.LoadLevel(5);
     }
 
-    public void SimultaneityExperimentScreen()
+    public void StartSimulationSimultaneity()
     {
-        Application.LoadLevel(6);
+        Application.LoadLevel(5);
     }
 
     public void Exit()
